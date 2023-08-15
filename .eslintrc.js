f867d02c2e86fbaebe81169d192411c97e3ccb49
yc <yc23702370@gmail.com>
2023-08-15 15:15:12 +0800

    install and set eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true, // 根目錄的設定文件
+  parser: '@typescript-eslint/parser', // 使用 TypeScript 解析器
+  plugins: ['@typescript-eslint', 'prettier'], // 使用的插件
+  extends: [
+    'eslint:recommended', // ESLint 推薦的規則
+    'plugin:@typescript-eslint/recommended', // TypeScript 相關的推薦規則
+    'plugin:prettier/recommended', // Prettier 推薦的規則
+  ],
+  rules: {
+    'prettier/prettier': 'error', // 要求符合 Prettier 的排版風格
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+}
